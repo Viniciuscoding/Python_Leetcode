eafd2de428212fd4462466093b7e4636b44160c9
Viniciuscoding <vsg232@nyu.edu>
2019-09-29 01:32:30 -0400

    Unique Number of Occurrences
    Given an array of integers arr, write a function that returns true if and only if the number of occurrences
    of each value in the array is unique.
    
    Example:
    
    Input: arr = [1,2,2,1,1,3]
    Output: true
    Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
    
    Constraints:
    1 <= arr.length <= 1000
    -1000 <= arr[i] <= 1000

--- Leetcode_1207_WC156.py
+++ Leetcode_1207_WC156.py
@@ -2,6 +2,7 @@
 Given an array of integers arr, write a function that returns true if and only if the number of occurrences
 of each value in the array is unique.
 
+Example:
 
 Input: arr = [1,2,2,1,1,3]
 Output: true
