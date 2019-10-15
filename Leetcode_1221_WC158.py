e2076f1d0c50756311228e7737f8f4cc13231b5b
Viniciuscoding <vsg232@nyu.edu>
2019-10-14 22:34:04 -0400

    Split a String in Balanced Strings
    Balanced strings are those who have equal quantity of 'L' and 'R' characters.
    Given a balanced string s split it in the maximum amount of balanced strings.
    Return the maximum amount of split balanced strings.
    
    Example 1:
    
    Input: s = "RLRRLLRLRL"
    Output: 4
    Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
    Example 2:
    
    Input: s = "RLLLLRRRLR"
    Output: 3
    Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
    Example 3:
    
    Input: s = "LLLLRRRR"
    Output: 1
    Explanation: s can be split into "LLLLRRRR".
    
    
    Constraints:
    1 <= s.length <= 1000
    s[i] = 'L' or 'R'

---
+++ Leetcode_1221_WC158.py
@@ -0,0 +1,47 @@
+"""
+Split a String in Balanced Strings
+
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+Given a balanced string s split it in the maximum amount of balanced strings.
+Return the maximum amount of splitted balanced strings.
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+
+
+Constraints:
+1 <= s.length <= 1000
+s[i] = 'L' or 'R'
+"""
+
+class Solution(object):
+    def balancedStringSplit(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count, count_R, count_L = 0,0,0
+        
+        for c in s:
+            if c == 'R':
+                count_R += 1            
+            else:
+                count_L += 1
+                
+            if count_R == count_L:
+                count += 1
+                count_R, count_L = 0,0
+        return count
