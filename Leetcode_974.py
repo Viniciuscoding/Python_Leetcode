b57121d596fb905bf61231bf492a06c2357e2772
Viniciuscoding <vsg232@nyu.edu>
2020-05-20 15:10:43 -0400

    Update Leetcode_974.py

--- Leetcode_974.py
+++ Leetcode_974.py
@@ -35,6 +35,18 @@ class Solution(object):
                 end = brek
         return count
         
+        # EXPLANATION
+        # 1. Since the array is continuos, loop through all the continuous combinations possible.
+        # 2. That was done by creating a while loop to go through all possible combinations and check everyone whose sum
+        #    was divisible by K.
+        # 2.1 Loop starts with one combination until the last combination includes all the elemements in the array. For example:
+        # 2.2 Combination 1: [4] -> [5] -> [0] -> [-2] -> [-3] -> [1] then increase the combination to two
+        # 2.3 Combination 2: [4, 5] -> [5,0] -> [0,-2] -> [-2,-3] -> [-3,1] then increase the combination to three
+        #      ...
+        # 2.4 Combination 5 (last one based on all the array elements): [4,5,0,-2,-3,1] then break the loop
+        # 3. Return the count of combinations whose sum is divisible by K
+        
+        
         # SOLUTION from Leetcode fastest
         res, prefix = 0, 0
         count = [1] + [0] * (K-1)
