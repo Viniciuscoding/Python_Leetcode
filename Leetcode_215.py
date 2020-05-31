53699289cc9eb5dcb955dbde3fe76a8443233f32
Viniciuscoding <vsg232@nyu.edu>
2020-05-31 03:58:01 -0400

    Kth Largest Element in an Array

--- Leetcode_215.py
+++ Leetcode_215.py
@@ -20,5 +20,16 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        sorted_nums = sorted(nums)
+        # SOLUTION descending order (A little faster since it is unecessary to find the length of the array)
+        sort_desc_nums = sorted(nums, reverse = True)
+        return sorted_nums[k - 1]
+        
+        # SOLUTION ascending order
+        sort_asc_nums = sorted(nums)
         return sorted_nums[len(nums) - k]
+
+# EXPLANATION
+# 1. Sort the array in descending order.
+# 2. Return the value at the index of the kth value. Keep in mind Python index starts at zero, so k must be subtracted by 1.
+# 3. You can also sove by sorting in ascending order. When you do that you have to find the kth position from the end to the
+#    the beginning by subtracting the array length with the kth value. Then return that index value.
