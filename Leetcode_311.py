c78a16747c6221c4876aaac7f4d64a0d418c590f
Viniciuscoding <vsg232@nyu.edu>
2020-05-31 03:26:22 -0400

    Sparse Matrix Multiplication - TRY AGAIN

---
+++ Leetcode_311.py
@@ -0,0 +1,52 @@
+"""
+Sparse Matrix Multiplication
+
+Given two sparse matrices A and B, return the result of AB.
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+Input:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+Output:
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+"""
+
+class Solution(object):
+    def multiply(self, A, B):
+        m, n, k = len(A), len(A[0]), len(B[0])
+        AB = [[0 for _ in range(k)] for _ in range(m)]
+        
+        if A is None or B is None: return None
+        
+        if len(B) != n:
+            print("A's column size must be equal to B's row size.")
+            return None
+        
+        # Pick up each row
+        for i, rowA in enumerate(A):
+
+             # Pick up each element of the row
+            for K, elA in enumerate(rowA):
+                # If the element is zero than ignore the calculation else calculate
+                if elA != 0:
+                    colB = B[K] # I am adding this variable for easier understanding
+                    for j, elB in enumerate(colB): # This is the column of B matrix
+                        if elB != 0:
+                            # Multiply each element and add them in their respective position
+                            AB[i][j] += elA * elB
+        return AB 
